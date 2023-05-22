7abe5e270925dfb5567f624bf2258efe3bcb3f6e
LuuDangCong <117263345+LuuDangCong@users.noreply.github.com>
2023-05-22 21:22:17 +0700

    Add files via upload

---
+++ AppAPI/Controllers/BinhLuanController.cs
@@ -0,0 +1,71 @@
+﻿using AppData.IRepositories;
+using AppData.Models;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BinhLuanController : ControllerBase
+    {
+        private IAllRepositories<BinhLuan> irepos;
+        private Du_An_NET105Context context= new Du_An_NET105Context();
+        public BinhLuanController()
+        {
+            AllRepositories<BinhLuan> repos =
+            new AllRepositories<BinhLuan>(context, context.BinhLuans);
+            irepos = repos;
+        }
+        // GET: api/<BinhLuan>
+        [HttpGet]
+        public IEnumerable<BinhLuan> Get()
+        {
+           return irepos.GetAll();
+        }
+
+        // GET api/<BinhLuan>/5
+        [HttpGet("{id}")]
+        public BinhLuan Get(Guid id)
+        {
+            return irepos.GetAll().First(p => p.IdbinhLuan == id);
+        }
+
+        // POST api/<BinhLuan>
+        [HttpPost("create-mausac")]
+        public bool CreateBL(string noidung)
+        {
+            BinhLuan BL = new BinhLuan();
+            BL.NoiDung = noidung;
+            BL.IdbinhLuan= Guid.NewGuid();
+            BL.Idspct= Guid.NewGuid();
+            BL.IdnguoiDung= Guid.NewGuid();
+          
+            return irepos.CreateItem(BL);
+        }
+        [HttpPut]
+        [Route("edit-mausac")]
+        public bool UpdateColor(Guid id, string noidung )
+        {
+            // Trỏ đến màu sắc trong db để sửa
+            
+            BinhLuan BL = irepos.GetAll().First(p => p.IdbinhLuan == id);
+            BL.NoiDung = noidung;
+            BL.IdbinhLuan = Guid.NewGuid();
+            BL.Idspct = Guid.NewGuid();
+            BL.IdnguoiDung = Guid.NewGuid();
+           
+            return irepos.UpdateItem(BL);
+        }
+        [HttpDelete("{id}")]
+        public bool DeleteColor(Guid id)
+        {
+            // Trỏ đến màu sắc trong db để sửa
+            BinhLuan BL = irepos.GetAll().First(p => p.IdbinhLuan == id);
+            return irepos.DeleteItem(BL);
+        }
+
+    }
+}
---
+++ AppAPI/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using AppData.IRepositories;
+using AppData.Models;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+  
+        public class UserController : ControllerBase
+        {
+            private IAllRepositories<NguoiDung> irepos;
+            private Du_An_NET105Context context = new Du_An_NET105Context();
+            public UserController()
+            {
+                AllRepositories<NguoiDung> repos = new AllRepositories<NguoiDung>(context, context.NguoiDungs);
+              irepos=repos;
+            }
+            // GET: api/<ValuesController>
+            [HttpGet]
+            public IEnumerable<NguoiDung> Get()
+            {
+                return irepos.GetAll();
+            }
+
+            // GET api/<ValuesController>/5
+            [HttpGet("{id}")]
+            public NguoiDung Get(Guid id)
+            {
+                return irepos.GetAll().First(p => p.Iduser == id);
+            }
+
+            // POST api/<ValuesController>
+            [HttpPost("create-người dùng")]
+            public bool CreateUser(string Tkh, string sdt, string email, string matkhau, string diachi, int trangthai)
+            {
+                NguoiDung user = new NguoiDung();
+                user.TenKhachHang = Tkh;
+                user.Sdt = sdt;
+                user.MatKhau = matkhau;
+                user.Email = email; 
+                user.TrangThai = trangthai;
+                user.DiaChi = diachi;
+                user.Iduser = Guid.NewGuid();
+                user.Idrole= Guid.NewGuid();
+                return irepos.CreateItem(user);
+            }
+
+            // PUT api/<ValuesController>/5
+            [HttpPut("{Update_user}")]
+            public bool UpdateUser(Guid id, string Tkh, string sdt, string email, string matkhau, string diachi, int trangthai)
+            {
+                NguoiDung user = irepos.GetAll().First(p => p.Iduser == id);
+                user.TenKhachHang = Tkh;
+                user.Sdt = sdt;
+                user.MatKhau = matkhau;
+                user.Email = email;
+                user.TrangThai = trangthai;
+                user.DiaChi = diachi;
+                return irepos.UpdateItem(user);
+            }
+
+            // DELETE api/<ValuesController>/5
+            [HttpDelete("{delete}")]
+            public bool DeleteUser(Guid id)
+            {
+                NguoiDung user = irepos.GetAll().First(p => p.Iduser == id);
+                return irepos.DeleteItem(user);
+            }
+        }
+    }
+
