8340b2bf3d7998ee4235c3b70d76144d8c0590b0
Dat01 <tangdat1801@gmail.com>
2023-05-23 23:27:46 +0700

    Update Ok

---
+++ AppAPI/Controllers/MauSacController.cs
@@ -0,0 +1,59 @@
+﻿using AppDaTa.IRepositories;
+using AppDaTa.Models;
+using AppDaTa.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppAPI.Controllers
+{
+
+    // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MauSacController : ControllerBase
+    {
+        private IAllRepositories<MauSac> ireposmausac;
+        private QLBG_Context _context = new QLBG_Context();
+        public MauSacController()
+        {
+            AllRepositories<MauSac> reposmausac = new AllRepositories<MauSac>(_context, _context.maus);
+            ireposmausac = reposmausac;
+        }
+        [HttpGet]
+        public IEnumerable<MauSac> GetAll()
+        {
+            return ireposmausac.GetAll();
+        }
+        [HttpGet("{id}")]
+        public MauSac Get(Guid id)
+        {
+            return ireposmausac.GetAll().First(c => c.IDMau == id);
+        }
+
+        // POST api/<SaleController>
+        [HttpPost("create-mausac")]
+        public bool CreateMauSac(string mausac)
+        {
+            MauSac mau = new MauSac();
+            mau.IDMau = Guid.NewGuid();
+            mau.Mausac = mausac;
+            return ireposmausac.CreateNewItem(mau);
+        }
+
+        // PUT api/<SaleController>/5
+        [HttpPut("{id}")]
+        public bool UpdateMauSac(Guid id, string mausac)
+        {
+            MauSac mau = ireposmausac.GetAll().First(c => c.IDMau == id);
+            mau.Mausac = mausac;
+            return ireposmausac.UpdateItem(mau);
+        }
+
+        // DELETE api/<SaleController>/5
+        [HttpDelete("{id}")]
+        public bool Delete(Guid id)
+        {
+            MauSac mau = ireposmausac.GetAll().First(c => c.IDMau == id);
+            return ireposmausac.DeleteItem(mau);
+        }
+    }
+}
