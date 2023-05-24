13b3eb825cf6b483ec0bbbd6852c2d31b4b8df04
LuuDangCong <117263345+LuuDangCong@users.noreply.github.com>
2023-05-24 07:00:00 +0700

    Update NguoiDungController.cs

--- AppAPI/Controllers/NguoiDungController.cs
+++ AppAPI/Controllers/NguoiDungController.cs
@@ -1,8 +1,9 @@
-﻿
+
 using AppDaTa.IRepositories;
 using AppDaTa.Models;
 using AppDaTa.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 
 namespace AppAPI.Controllers
 {
@@ -13,13 +14,16 @@ namespace AppAPI.Controllers
     {
         private IAllRepositories<NguoiDung> ireposUser;
         private IAllRepositories<ChucVu> ireposRole;
+     
         private QLBG_Context context = new QLBG_Context();
         public NguoiDungController()
         {
             AllRepositories<NguoiDung> reposuser = new AllRepositories<NguoiDung>(context, context.nguoiDungs);
             AllRepositories<ChucVu> reposrole = new AllRepositories<ChucVu>(context, context.chucVus);
+           
             ireposUser = reposuser;
             ireposRole = reposrole;
+            
         }
         // GET: api/<ValuesController>
         [HttpGet]
@@ -28,16 +32,12 @@ namespace AppAPI.Controllers
             return ireposUser.GetAll();
         }
 
-        // GET api/<ValuesController>/5
-        //[HttpGet("{id}")]
-        //public bool NguoiDung Get(Guid id)
-        //{
-        //    return irepos.GetAll().First(p => p.Iduser == id);
-        //}
+     
+        
 
         // POST api/<ValuesController>
         [HttpPost("create-user")]
-        public bool CreateUser(string Tkh, string sdt, string email, string matkhau, string diachi)
+        public bool CreateUser(Guid idrole,Guid giohang,string Tkh, string sdt, string email, string matkhau, string diachi)
         {
             NguoiDung user = new NguoiDung();
             user.IDUser = Guid.NewGuid();
@@ -47,15 +47,17 @@ namespace AppAPI.Controllers
             user.Email = email;
             user.TrangThai = 1;
             user.DiaChi = diachi;
-            user.IDRole= ireposRole.GetAll().First(c => c.TenChucVu == "Khach hang").IDRole;
+            user.IDRole= ireposRole.GetAll().First(c => c.IDRole == idrole).IDRole;
+
             return ireposUser.CreateNewItem(user);
         }
 
         // PUT api/<ValuesController>/5
         [HttpPut("Update_user")]
-        public bool UpdateUser(Guid id, string Tkh, string sdt, string email, string matkhau, string diachi, int trangthai)
+        public bool UpdateUser(Guid id, Guid idrole, string Tkh, string sdt, string email, string matkhau, string diachi, int trangthai)
         {
-            NguoiDung user = ireposUser.GetAll().First(p => p.IDUser == id);
+            NguoiDung user = ireposUser.GetAll().FirstOrDefault(p => p.IDUser == id);
+            user.IDRole = ireposRole.GetAll().First(c => c.IDRole == idrole).IDRole;
             user.TenKhachHang = Tkh;
             user.SDT = sdt;
             user.MatKhau = matkhau;
